601440efcb2f28f6d64c38ac3d7bd1f04e4fa1b2
Melanie Tolvett S <melanietolvett@gmail.com>
2023-02-01 11:01:14 -0300

    limpiando main

--- index.js
+++ index.js
@@ -2,22 +2,4 @@
 //   //
 // };
 
-  const fs = require('fs');
-
-  //console.log(fs);
-
-  //---------Leyando un archivo md con ruta fija
-  const readFileFn = (path) => {
-    fs.readFile(path, 'utf-8', (error, data) => {
-      if (!error) {
-        console.log(data);
-      } else {
-        console.log(`Error: ${error}`);
-      }
-    });
-  };
-  readFileFn('README.md');
-
-module.exports = {readFileFn}
-
-
+module.exports = {}
